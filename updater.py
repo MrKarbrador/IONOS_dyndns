9e828cabcfeaaff70ad0424b58b2a63b6b6e6fc8
Simon <simon.zuern@uni-wuppertal.de>
2021-11-07 14:44:46 +0100

    added test parameter

--- updater.py
+++ updater.py
@@ -2,6 +2,7 @@ import socket
 from requests import get, post
 import logging
 import os
+import sys
 
 domains = [
     'example.com'
@@ -24,7 +25,7 @@ def test_ip() -> bool:
     logging.info(f"To update: {str(to_update)}")
     return to_update
 
-def update_ips(list_domains: list[str]):
+def update_ips(list_domains):
     header={'x-api-key': X_API_KEY}
     post_param = {
         'domains': list_domains,
@@ -38,5 +39,8 @@ def update_ips(list_domains: list[str]):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     update_list = test_ip()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'test':
+            update_list = domains
     if len(update_list) > 0:
-        update_ips(list_domains)
+        update_ips(update_list)
