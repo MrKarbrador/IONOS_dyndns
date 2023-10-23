d532c8a9ccf21f414f7745f9a722b8055c2bd4e6
Simon Zürn <simon.zuern@uni-wuppertal.de>
2023-10-23 19:35:10 +0200

    add ipv6 support

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ class Config:
         'www.example.com'
     ]
     own_ip_api_url = 'https://api.ipify.org/'
+    own_ipv6_api_url = 'https://api64.ipify.org/'
     x_api_key = 'key'
     ionos_api = 'https://api.hosting.ionos.com/dns'
 
